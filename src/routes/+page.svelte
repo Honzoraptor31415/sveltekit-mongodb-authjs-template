ded7d77c2e4911eb1c771685b8b2916126c380f4
Honzoraptor <j.ackermann@email.cz>
2024-08-21 16:15:26 +0200

    Added some icons and once again improved design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,30 @@
 <script>
 	import BlurryBackground from '$lib/components/ui/BlurryBackground.svelte';
-	import GitHub from '$lib/icons/GitHub.svelte';
+	import GitHubIcon from '$lib/icons/GitHubIcon.svelte';
+	import SvelteKitIcon from '$lib/icons/skill-icons/SvelteKitIcon.svelte';
+	import MongoDBIcon from '$lib/icons/skill-icons/MongoDBIcon.svelte';
+	import AuthjsIcon from '$lib/icons/skill-icons/AuthjsIcon.svelte';
 </script>
 
 <header class="flex items-center justify-center min-h-[100svh]">
 	<BlurryBackground />
 	<div class="flex flex-col items-center gap-6 header-content">
-		<h1 class="text-center">SvelteKit + MongoDB + Auth.js</h1>
+		<h1 class="flex gap-3 items-center flex-wrap justify-center">
+			<div class="flex gap-3 items-center">
+				<span>SvelteKit</span>
+				<SvelteKitIcon iconClass="max-h-8 w-auto" />
+			</div>
+			+
+			<div class="flex gap-3 items-center">
+				<span>MongoDB</span>
+				<MongoDBIcon iconClass="max-h-8 w-auto" />
+			</div>
+			+
+			<div class="flex gap-3 items-center">
+				<span>Auth.js</span>
+				<AuthjsIcon iconClass="max-h-8 w-auto" />
+			</div>
+		</h1>
 		<div class="flex gap-4">
 			<a
 				href="https://github.com/new?template_name=sveltekit-mongodb-authjs-template&template_owner=Honzoraptor31415"
@@ -16,9 +34,11 @@
 				href="https://github.com/Honzoraptor31415/sveltekit-mongodb-authjs-template/"
 				class="button secondary-button flex items-center gap-2"
 			>
-				<GitHub iconClass="max-h-5 w-auto" />
+				<GitHubIcon iconClass="max-h-5 w-auto" />
 				<span>View GitHub</span>
 			</a>
 		</div>
 	</div>
 </header>
+
+<main></main>
