69f1f850faa45ce4bc5b650c3cbfb621326a69bb
Honzoraptor <j.ackermann@email.cz>
2024-08-21 12:33:39 +0200

    Fixed design issues

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,16 +2,14 @@
 	import GitHub from "$lib/icons/GitHub.svelte";
 
 </script>
-<header class="flex align-middle min-h-[100svh]">
-  <div class="flex flex-col gap-2 align-center">
-    <div class="heading-wrp">
+<header class="flex items-center justify-center min-h-[100svh]">
+  <div class="flex flex-col items-center gap-6">
       <h1>SvelteKit + MongoDB + Auth.js</h1>
-      <span class="text">Template</span>
-    </div>
-    <div class="gap-4">
-      <a href="https://github.com/new?template_name=sveltekit-mongodb-authjs-template&template_owner=Honzoraptor31415" class="button">Use template</a>
-      <a href="https://github.com/Honzoraptor31415/sveltekit-mongodb-authjs-template/" class="button gap-2">
-      <GitHub iconClass="max-h-5" />
+    <div class="flex gap-4">
+      <a href="https://github.com/new?template_name=sveltekit-mongodb-authjs-template&template_owner=Honzoraptor31415" class="button primary-button">Use template</a>
+      <a href="https://github.com/Honzoraptor31415/sveltekit-mongodb-authjs-template/" class="button secondary-button flex items-center gap-2">
+      <GitHub iconClass="max-h-5 w-auto" />
+      <span>View GitHub</span>
       </a>
     </div>
   </div>
