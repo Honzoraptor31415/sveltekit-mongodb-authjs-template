9f7c2de7e8f4078caee864dcf9bb02395350524b
Honzoraptor <j.ackermann@email.cz>
2024-08-21 15:25:50 +0200

    Added more design

---
+++ src/lib/components/ui/BlurryBackground.svelte
@@ -0,0 +1,3 @@
+<div class="absolute inset-0 -z-10 flex items-center justify-center">
+	<div class="blurry-element"></div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,24 @@
 <script>
-	import GitHub from "$lib/icons/GitHub.svelte";
-
+	import BlurryBackground from '$lib/components/ui/BlurryBackground.svelte';
+	import GitHub from '$lib/icons/GitHub.svelte';
 </script>
+
 <header class="flex items-center justify-center min-h-[100svh]">
-  <div class="flex flex-col items-center gap-6">
-      <h1>SvelteKit + MongoDB + Auth.js</h1>
-    <div class="flex gap-4">
-      <a href="https://github.com/new?template_name=sveltekit-mongodb-authjs-template&template_owner=Honzoraptor31415" class="button primary-button">Use template</a>
-      <a href="https://github.com/Honzoraptor31415/sveltekit-mongodb-authjs-template/" class="button secondary-button flex items-center gap-2">
-      <GitHub iconClass="max-h-5 w-auto" />
-      <span>View GitHub</span>
-      </a>
-    </div>
-  </div>
+	<BlurryBackground />
+	<div class="flex flex-col items-center gap-6 header-content">
+		<h1 class="text-center">SvelteKit + MongoDB + Auth.js</h1>
+		<div class="flex gap-4">
+			<a
+				href="https://github.com/new?template_name=sveltekit-mongodb-authjs-template&template_owner=Honzoraptor31415"
+				class="button primary-button">Use template</a
+			>
+			<a
+				href="https://github.com/Honzoraptor31415/sveltekit-mongodb-authjs-template/"
+				class="button secondary-button flex items-center gap-2"
+			>
+				<GitHub iconClass="max-h-5 w-auto" />
+				<span>View GitHub</span>
+			</a>
+		</div>
+	</div>
 </header>
