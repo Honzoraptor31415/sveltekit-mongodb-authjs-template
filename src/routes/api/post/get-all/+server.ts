2c1b4f7bdc0629359fb2309ef887de7fec2b7313
Honzoraptor <j.ackermann@email.cz>
2024-08-31 15:53:23 +0200

    Secured /api/post/get-all from being accessec by typing the url in the browser

--- src/routes/api/post/get-all/+server.ts
+++ src/routes/api/post/get-all/+server.ts
@@ -1,8 +1,12 @@
 import database from '$lib/database';
-import { json, type RequestHandler } from '@sveltejs/kit';
+import { error, json, type RequestHandler } from '@sveltejs/kit';
 
 export const GET: RequestHandler = async (event) => {
-	console.log(event);
+	const isRequestFromBrowserWindow = event.request.headers.get('sec-fetch-user') !== null;
+
+	if (isRequestFromBrowserWindow) {
+		error(404);
+	}
 
 	const posts = await database.collection('posts').find().toArray();
 
